7bd2b9956382857d349d0eb7a8574cbd14be0018
moeelnady <mohamedelnady007@gmail.com>
2025-04-21 23:05:51 +0200

    "changing ports"

--- Backend/authentication-service/src/main/java/telemedaid/authentication_service/Config/PatientServiceClient.java
+++ Backend/authentication-service/src/main/java/telemedaid/authentication_service/Config/PatientServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import telemedaid.authentication_service.DTOs.CreatePatientRequest;
 
-@FeignClient(name = "patient-service", url = "http://localhost:8081")
+@FeignClient(name = "patient-service", url = "http://localhost:8082")
 public interface PatientServiceClient {
 
     @PostMapping("/patients/register")
