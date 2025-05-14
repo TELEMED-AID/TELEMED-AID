fbbcd0af4bb2d68f6b6e2200fa33248fed8448e0
Mahmoud Ghallab <randyorten9055@gmail.com>
2025-05-14 04:40:00 +0300

    milestone#1-Final commit with component tests

---
+++ Backend/src/main/java/com/appointments/appointmentservice/DTOs/AppointmentResponseDTO.java
@@ -0,0 +1,20 @@
+package com.appointments.appointmentservice.DTOs;
+
+import com.appointments.appointmentservice.Entities.AppointmentState;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Builder
+public class AppointmentResponseDTO {
+    private String userId;
+    private DoctorDataDTO doctorDetails;
+    private LocalDate date;
+    private LocalTime time;
+    private AppointmentState state;
+}
