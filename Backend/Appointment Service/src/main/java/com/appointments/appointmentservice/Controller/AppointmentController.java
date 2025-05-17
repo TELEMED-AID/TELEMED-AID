e9de39e30456e276c5824c16ec808e5ab3a07ce5
Mahmoud Ghallab <randyorten9055@gmail.com>
2025-05-17 20:22:34 +0300

    Update AppointmentController.java

--- Backend/Appointment Service/src/main/java/com/appointments/appointmentservice/Controller/AppointmentController.java
+++ Backend/Appointment Service/src/main/java/com/appointments/appointmentservice/Controller/AppointmentController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/appointments")
 @RequestMapping("/api/appointment")
 @RequiredArgsConstructor
 public class AppointmentController {
