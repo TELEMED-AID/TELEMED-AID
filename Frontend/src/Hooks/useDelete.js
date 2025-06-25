ce990f40f008f48958b1cadad7548cef1510517b
kmmas <khaledmohamad362@gmail.com>
2025-06-25 16:26:13 +0300

    add available days and delete button for doctor

--- Frontend/src/Hooks/useDelete.js
+++ Frontend/src/Hooks/useDelete.js
@@ -8,7 +8,7 @@ const useDelete = () => {
     const [loading, setLoading] = useState(false);
 
     // Added 'data' parameter here
-    const deleteItem = (
+    const deleteItem = async (
         url,
         data,
         deleteCallback,
@@ -19,39 +19,38 @@ const useDelete = () => {
     ) => {
         setLoading(true);
         // Pass 'data' as the second argument to axiosInstance.delete
-        return axiosInstance
-            .delete(url, { data }) // axios.delete can accept a config object with a 'data' key for the request body
-            .then((response) => {
-                setLoading(false);
-                if (successMessage) {
-                    dispatch(
-                        setSnackbar({
-                            open: showSnackbar,
-                            message: successMessage,
-                            error: false,
-                        })
-                    );
-                }
-                deleteCallback(response.data);
-                return response.data;
-            })
-            .catch((error) => {
+        try {
+            const response = await axiosInstance
+                .delete(url, { data }) // axios.delete can accept a config object with a 'data' key for the request body
+                ;
+            setLoading(false);
+            if (successMessage) {
                 dispatch(
                     setSnackbar({
-                        open: true,
-                        message:
-                            errorMessage ||
-                            error.response?.data?.message ||
-                            "Internal Server Error!",
-                        error: true,
+                        open: showSnackbar,
+                        message: successMessage,
+                        error: false,
                     })
                 );
-                setLoading(false);
-                if (errorCallback) {
-                    errorCallback(error);
-                }
-                return false;
-            });
+            }
+            deleteCallback(response.data);
+            return response.data;
+        } catch (error) {
+            dispatch(
+                setSnackbar({
+                    open: true,
+                    message: errorMessage ||
+                        error.response?.data?.message ||
+                        "Internal Server Error!",
+                    error: true,
+                })
+            );
+            setLoading(false);
+            if (errorCallback) {
+                errorCallback(error);
+            }
+            return false;
+        }
     };
 
     return { loading, deleteItem };
